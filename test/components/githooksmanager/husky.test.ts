230fe27c0a76c354cdc6a97dff7ff1073a02eefb
Thomas Roche <thomas.roche@postcodelottery.co.uk>
2023-08-30 12:50:15 +0100

    test: husky singleton and dev deps

--- test/components/githooksmanager/husky.test.ts
+++ test/components/githooksmanager/husky.test.ts
@@ -1,14 +1,44 @@
 import { synthSnapshot } from "projen/lib/util/synth";
 import { GitHooksEnabledProject } from "../../../src";
-import { GitHooksManagerType } from "../../../src/components/githooksmanager";
+import { GitHooksManagerType, Husky } from "../../../src/components/githooksmanager";
 
 describe("Husky", () => {
-  it("has a new pre-commit file", () => {
+  it("Retuns a singleton", () => {
     // Arrange
     const project = new GitHooksEnabledProject({
       name: "test",
       defaultReleaseBranch: "main",
-      prettier: true,
+      gitHooksManager: GitHooksManagerType.HUSKY,
+    });
+
+    // Act
+    const husky = project.gitHooksManager;
+
+    // Assert
+    expect(husky).toEqual(Husky.of(project));
+  });
+
+  it("Adds new dev dependencies", () => {
+    // Arrange
+    const project = new GitHooksEnabledProject({
+      name: "test",
+      defaultReleaseBranch: "main",
+      gitHooksManager: GitHooksManagerType.HUSKY,
+    });
+
+    // Act
+    const snapshot = synthSnapshot(project);
+    const config = snapshot["package.json"].devDependencies;
+
+    // Assert
+    expect(Object.keys(config)).toContain("husky");
+  });
+
+  it("Adds a new pre-commit file", () => {
+    // Arrange
+    const project = new GitHooksEnabledProject({
+      name: "test",
+      defaultReleaseBranch: "main",
       gitHooksManager: GitHooksManagerType.HUSKY,
     });
 
@@ -20,12 +50,11 @@ describe("Husky", () => {
     expect(config).toEqual('#!/bin/sh\n. "$(dirname "$0")/_/husky.sh"\n\nnpx lint-staged');
   });
 
-  it("has a new pre-push file", () => {
+  it("Adds a new pre-push file", () => {
     // Arrange
     const project = new GitHooksEnabledProject({
       name: "test",
       defaultReleaseBranch: "main",
-      prettier: true,
       gitHooksManager: GitHooksManagerType.HUSKY,
     });
 
