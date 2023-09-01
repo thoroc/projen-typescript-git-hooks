fd9330b4215f49da599765987886ea952b639783
Thomas Roche <thomas.roche@postcodelottery.co.uk>
2023-09-01 14:27:53 +0100

    style: regenerating project files

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { javascript } from "projen";
+import { YamlFile, javascript } from "projen";
 import { GitHooksManagerType } from "./src/components/githooksmanager";
 import { GitHooksEnabledProject } from "./src/projects";
 
@@ -20,4 +20,23 @@ const project = new GitHooksEnabledProject({
 
   gitHooksManager: GitHooksManagerType.HUSKY,
 });
+
+new YamlFile(project, ".github/workflows/pull-request-comments.yml", {
+  obj: {
+    name: "Jest Coverage Comment",
+    on: { pull_request: "" },
+    jobs: {
+      build: {
+        "runs-on": "ubuntu-latest",
+        steps: [
+          { uses: "actions/checkout@v3" },
+          { name: "Install dependencies", run: "npx project install:ci" },
+          { name: "Run tests", run: "npx jest --coverage --coverageReporters json-summary" },
+          { name: "Jest Coverage Comment", uses: "MishaKav/jest-coverage-comment@main" },
+        ],
+      },
+    },
+  },
+});
+
 project.synth();
