92332c6857c426e6f7d5c7118c5d325058e281a8
Thomas Roche <thomas.roche@postcodelottery.co.uk>
2023-09-01 12:26:28 +0100

    feat: new code standards: editorconfig

---
+++ src/components/codestandards/editorconfig/params.ts
@@ -0,0 +1,35 @@
+import { SnakeCasedPropertiesDeep } from "type-fest";
+
+export interface EditorConfigParamsOptions {
+  indentStyle?: string;
+  indentSize?: number;
+  endOfLine?: string;
+  charset?: string;
+  insertFinalNewline?: boolean;
+}
+
+export class EditorConfigParams implements EditorConfigParamsOptions {
+  readonly indentStyle?: string;
+  readonly indentSize?: number;
+  readonly endOfLine?: string;
+  readonly charset?: string;
+  readonly insertFinalNewline?: boolean;
+
+  constructor(options: EditorConfigParamsOptions) {
+    this.indentStyle = options.indentStyle;
+    this.indentSize = options.indentSize;
+    this.endOfLine = options.endOfLine;
+    this.charset = options.charset;
+    this.insertFinalNewline = options.insertFinalNewline;
+  }
+
+  serialise(): SnakeCasedPropertiesDeep<EditorConfigParamsOptions> {
+    return {
+      indent_style: this.indentStyle,
+      indent_size: this.indentSize,
+      end_of_line: this.endOfLine,
+      charset: this.charset,
+      insert_final_newline: this.insertFinalNewline,
+    };
+  }
+}
