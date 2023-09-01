f007f010f3f5e4397c7eaf9bf26bf29fa50cdd37
Thomas Roche <thomas.roche@postcodelottery.co.uk>
2023-09-01 13:58:16 +0100

    feat: enabling markdownlint from base project class

--- src/components/codestandards/markdownlint/index.ts
+++ src/components/codestandards/markdownlint/index.ts
@@ -1,6 +1,8 @@
-import { Component, IgnoreFile, Project, SourceCode, YamlFile, typescript } from "projen";
+import { Component, IgnoreFile, Project, SourceCode, YamlFile } from "projen";
 import { MarkdownlintRules } from "./rules";
+import { GitHooksEnabledProject } from "../../../projects";
 import { recursiveToSnake, toSnakeCase } from "../../../utils";
+import { Husky, LintStaged } from "../../githooksmanager";
 
 export interface MarkdownlintOptions {
   /**
@@ -17,18 +19,7 @@ export interface MarkdownlintOptions {
 }
 
 export class Markdownlint extends Component {
-  /**
-   * Returns the singletone component of a project or undefined if there is none.
-   */
-  public static of(project: Project): Markdownlint | undefined {
-    const singleton = (c: Component): c is Markdownlint => c instanceof Markdownlint;
-    return project.components.find(singleton);
-  }
-
-  readonly project: typescript.TypeScriptProject;
-  readonly options: MarkdownlintOptions;
-
-  private readonly defaultRules = {
+  static defaultRules = {
     MD013: {
       lineLength: 80,
       headingLineLength: 80,
@@ -47,6 +38,17 @@ export class Markdownlint extends Component {
   };
 
   /**
+   * Returns the singletone component of a project or undefined if there is none.
+   */
+  public static of(project: Project): Markdownlint | undefined {
+    const singleton = (c: Component): c is Markdownlint => c instanceof Markdownlint;
+    return project.components.find(singleton);
+  }
+
+  readonly project: GitHooksEnabledProject;
+  readonly options: MarkdownlintOptions;
+
+  /**
    * Direct access to the markdownlint rules (escape hatch)
    */
   public rules: MarkdownlintRules;
@@ -56,11 +58,11 @@ export class Markdownlint extends Component {
    */
   public readonly ignoreFile?: IgnoreFile;
 
-  constructor(project: typescript.TypeScriptProject, options?: MarkdownlintOptions) {
+  constructor(project: GitHooksEnabledProject, options?: MarkdownlintOptions) {
     super(project);
     this.project = project;
     this.options = options ?? {};
-    this.rules = options?.rules ?? this.defaultRules;
+    this.rules = options?.rules ?? Markdownlint.defaultRules;
 
     if (options?.ignoreFile ?? true) {
       this.ignoreFile = new IgnoreFile(project, ".markdownlintignore");
@@ -73,6 +75,13 @@ export class Markdownlint extends Component {
 
       new YamlFile(this.project, "markdownlint.yml", {
         obj: transformedRules,
+      });
+    }
+
+    if (this.project.gitHooksManager instanceof Husky) {
+      LintStaged.of(this.project)?.addRule({
+        filePattern: "*.md",
+        commands: ["markdownlint-cli2 --fix"],
       });
     }
   }
