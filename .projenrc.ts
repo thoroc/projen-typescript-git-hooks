57cdaaae6a013eae4e64e0524d0f7333d12cc042
Thomas Roche <thomas.a.roche@gmail.com>
2023-09-02 20:19:19 +0100

    feat: re-introduced PR comment Github Action

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { cdk } from 'projen';
+import { YamlFile, cdk } from 'projen';
 
 const project = new cdk.JsiiProject({
   author: 'thoroc',
@@ -15,6 +15,32 @@ const project = new cdk.JsiiProject({
   // packageName: undefined,  /* The "name" in package.json. */
   npmDistTag: "latest",
   npmRegistryUrl: "https:///npm.pkg.github.com"
+});
+
+new YamlFile(project, ".github/workflows/pull-request-comments.yml", {
+  obj: {
+    name: "Jest Coverage Comment",
+    on: { pull_request: {} },
+    jobs: {
+      build: {
+        "runs-on": "ubuntu-latest",
+        env: { CI: "true" },
+        steps: [
+          {
+            name: "Checkout",
+            uses: "actions/checkout@v3",
+            with: {
+              ref: "${{ github.event.pull_request.head.ref }}",
+              repository: "${{ github.event.pull_request.head.repo.full_name }}",
+            },
+          },
+          { name: "Install dependencies", run: "yarn install --check-files" },
+          { name: "Run tests", run: "npx jest --coverage --coverageReporters json-summary" },
+          { name: "Jest Coverage Comment", uses: "MishaKav/jest-coverage-comment@main" },
+        ],
+      },
+    },
+  },
 });
 
 project.synth();
