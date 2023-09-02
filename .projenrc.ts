39dacff77c135d6d9b131e46d1633ede506d258e
Thomas Roche <thomas.roche@****>
2023-09-02 19:32:46 +0100

    feat: moving to jsii project type for the repo

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,50 +1,48 @@
-import { YamlFile, javascript } from "projen";
-import { GitHooksManagerType } from "./src/components/githooksmanager";
-import { GitHooksEnabledProject } from "./src/projects";
+import { cdk, javascript } from "projen";
 
-const project = new GitHooksEnabledProject({
-  defaultReleaseBranch: "main",
-  name: "projen-typescript-git-hooks",
-  repository: "https://github.com/thoroc/projen-typescript-git-hooks",
+const project = new cdk.JsiiProject({
+  name: "@thoroc/projen-typescript-git-hooks",
   description: "Projen template for Typescript project supporting Git hooks and extra tooling",
+  keywords: ["projen", "typescript", "husky", "lefthook"],
+  defaultReleaseBranch: "main",
+  repositoryUrl: "https://github.com/thoroc/projen-typescript-git-hooks",
   projenrcTs: true,
 
-  peerDeps: ["projen"],
+  author: "thoroc",
+  authorAddress: "thomas.a.roche@gmail.com",
+
   deps: ["projen"],
-  debug: true,
-  docgen: true,
+  peerDeps: ["projen"],
 
-  autoApproveUpgrades: true,
-  autoApproveOptions: { allowedUsernames: ["thoroc"], secret: "GITHUB_TOKEN" },
-  depsUpgradeOptions: { workflowOptions: { schedule: javascript.UpgradeDependenciesSchedule.WEEKLY } },
-
-  gitHooksManager: GitHooksManagerType.HUSKY,
+  release: true,
+  npmDistTag: 'latest',
+  npmRegistryUrl: 'https://npm.pkg.github.com',
 });
 
-new YamlFile(project, ".github/workflows/pull-request-comments.yml", {
-  obj: {
-    name: "Jest Coverage Comment",
-    on: { pull_request: {} },
-    jobs: {
-      build: {
-        "runs-on": "ubuntu-latest",
-        env: { CI: "true" },
-        steps: [
-          {
-            name: "Checkout",
-            uses: "actions/checkout@v3",
-            with: {
-              ref: "${{ github.event.pull_request.head.ref }}",
-              repository: "${{ github.event.pull_request.head.repo.full_name }}",
-            },
-          },
-          { name: "Install dependencies", run: "yarn install --check-files" },
-          { name: "Run tests", run: "npx jest --coverage --coverageReporters json-summary" },
-          { name: "Jest Coverage Comment", uses: "MishaKav/jest-coverage-comment@main" },
-        ],
-      },
-    },
-  },
-});
+// new YamlFile(project, ".github/workflows/pull-request-comments.yml", {
+//   obj: {
+//     name: "Jest Coverage Comment",
+//     on: { pull_request: {} },
+//     jobs: {
+//       build: {
+//         "runs-on": "ubuntu-latest",
+//         env: { CI: "true" },
+//         steps: [
+//           {
+//             name: "Checkout",
+//             uses: "actions/checkout@v3",
+//             with: {
+//               ref: "${{ github.event.pull_request.head.ref }}",
+//               repository: "${{ github.event.pull_request.head.repo.full_name }}",
+//             },
+//           },
+//           { name: "Install dependencies", run: "yarn install --check-files" },
+//           { name: "Run tests", run: "npx jest --coverage --coverageReporters json-summary" },
+//           { name: "Jest Coverage Comment", uses: "MishaKav/jest-coverage-comment@main" },
+//         ],
+//       },
+//     },
+//   },
+// });
 
 project.synth();
